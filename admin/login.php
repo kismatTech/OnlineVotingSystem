76bbf0f47a9b3460e04029b8b70b8c383ec11fa1
Kismat-Tech <70833512+kismatTech@users.noreply.github.com>
2024-01-31 17:39:43 +0545

    Add files via upload

--- admin/login.php
+++ admin/login.php
@@ -16,6 +16,7 @@ if (isset($_POST['login'])) {
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="asset/css/login.css">
@@ -24,20 +25,76 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Voting System</title>
+    <link href="../assets/img/favicon.png" rel="icon">
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
+    <!-- ======= Top Bar ======= -->
+    <section id="topbar" class="d-flex align-items-center">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center">
+                <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@eVoteshield.com">contact@eVoteshield.com</a></i>
+                <i class="bi bi-phone d-flex align-items-center ms-4"><span>974-4304140</span></i>
+            </div>
+
+            <div class="cta d-none d-md-flex align-items-center">
+                <a href="login.php" class="scrollto">Login</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="mh-100 logo">
+                <!-- <h1><a href="index.html">eVote Shield</a></h1> -->
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <a href="../index.html"><img src="../assets/img/evote.png" alt="eVote Shield" class="img-fluid"></a>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="../index.html#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="../index.html#about">About</a></li>
+                    <li><a class="nav-link scrollto" href="../index.html#services">Services</a></li>
+                    <!-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li> -->
+                    <li><a class="nav-link scrollto" href="../index.html#team">Team</a></li>
+                    <!-- <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li> -->
+                    <!-- <li><a href="blog.html">Blog</a></li> -->
+                    <li><a class="nav-link scrollto" href="../index.html#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
 
     <div class="container">
-        <div class="card card-login mx-auto text-center bg-dark">
-            <div class="card-header mx-auto bg-dark">
-                <span> <img src="asset/image/black-logo.png" class="w-50" alt="eVote Shield"> </span><br />
+        <div class="card card-login mx-auto text-center bg-light">
+            <div class="card-header mx-auto bg-light">
+                <span> <img src="asset/image/white-logo.png" class="w-50" alt="eVote Shield"> </span><br />
                 <!-- <span class="logo_title mt-50"> Login Dashboard </span> -->
             </div>
             <?php
-            if (isset($_GET['sign-up'])) {  
+            if (isset($_GET['sign-up'])) {
             ?>
                 <div class="card-body">
                     <form action="" method="POST">
